e79a8925e815fdabf5d0817c96da3ea13e61eefb
bgk- <bgk@Blakes-MacBook-Pro.local>
2019-04-04 18:16:56 -0700

    Updates

---
+++ precache-manifest.889ba15a518082800528b8a57ca4351b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "800e3db097704765ee50",
+    "url": "/topiary/static/js/runtime~main.800e3db0.js"
+  },
+  {
+    "revision": "465fa3d3b02616442f48",
+    "url": "/topiary/static/js/main.465fa3d3.chunk.js"
+  },
+  {
+    "revision": "16f92ee0d01945a8a035",
+    "url": "/topiary/static/js/2.16f92ee0.chunk.js"
+  },
+  {
+    "revision": "465fa3d3b02616442f48",
+    "url": "/topiary/static/css/main.43ae7074.chunk.css"
+  },
+  {
+    "revision": "5f5cc91577fdd1d3ac40fab4ddd307c4",
+    "url": "/topiary/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "https://peartreegames.github.io/topiary/precache-manifest.8192a59444206c0b74b6a152c340d0e1.js"
+  "/topiary/precache-manifest.889ba15a518082800528b8a57ca4351b.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("https://peartreegames.github.io/topiary/index.html", {
+workbox.routing.registerNavigationRoute("/topiary/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
