33bfa6ec1fc727b761a281cbe453f9585ce4397e
bgk- <bgk@Blakes-MacBook-Pro.local>
2019-04-04 17:21:42 -0700

    Include build in docs folder

---
+++ docs/precache-manifest.81d37e405cca5a0789555fe3b3058f3e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "e87276bec28e361294b6",
+    "url": "/static/js/main.e87276be.chunk.js"
+  },
+  {
+    "revision": "1cc736ca78955fb6eabb",
+    "url": "/static/js/2.1cc736ca.chunk.js"
+  },
+  {
+    "revision": "e87276bec28e361294b6",
+    "url": "/static/css/main.43ae7074.chunk.css"
+  },
+  {
+    "revision": "fd2f6091a026bd4be61bc02bec23cc1a",
+    "url": "/index.html"
+  }
+];
