6c413a64aede86367e6c10b70ec56a29f11e1b13
bgk- <blakegraysonkehler@gmail.com>
2023-10-01 16:54:45 -0700

    First draft of ExportValue collections

--- src/export-value.zig
+++ src/export-value.zig
@@ -7,6 +7,8 @@ const Tag = enum(u8) {
     number,
     string,
     list,
+    set,
+    map,
 };
 
 pub const ExportValue = extern struct {
@@ -31,13 +33,36 @@ pub const ExportValue = extern struct {
             .bool => |b| if (b) True else False,
             .number => |n| .{ .tag = Tag.number, .data = .{ .number = n } },
             .obj => |o| switch (o.data) {
+                // We're mixing memory management here, which is a very bad idea,
+                // I'm just unsure of a better way currently.
+                // Maybe pass in a function pointer to the "add" of the managed collection type?
                 .string => |s| .{ .tag = Tag.string, .data = .{ .string = s.ptr } },
                 .list => |l| blk: {
                     var list = allocator.alloc(ExportValue, l.items.len) catch @panic("Could not allocate list items");
-                    for (l.items, 0..) |item, i| {
-                        list[i] = fromValue(item, allocator);
+                    var i: usize = 0;
+                    while (i < l.items.len) : (i += 1) {
+                        list[i] = fromValue(l.items[i], allocator);
                     }
                     break :blk .{ .tag = Tag.list, .data = .{ .list = .{ .items = list.ptr, .count = @intCast(list.len) } } };
+                },
+                .set => |s| blk: {
+                    var list = allocator.alloc(ExportValue, s.count()) catch @panic("Could not allocate list items");
+                    for (s.keys(), 0..) |key, i| {
+                        list[i] = fromValue(key, allocator);
+                    }
+                    break :blk .{ .tag = Tag.set, .data = .{ .list = .{ .items = list.ptr, .count = @intCast(list.len) } } };
+                },
+                .map => |m| blk: {
+                    var count = m.count();
+                    var list = allocator.alloc(ExportValue, count * 2) catch @panic("Could not allocate list items");
+                    var it = m.iterator();
+                    var i: usize = 0;
+                    while (it.next()) |kvp| {
+                        list[i] = fromValue(kvp.key_ptr.*, allocator);
+                        list[i + 1] = fromValue(kvp.value_ptr.*, allocator);
+                        i += 2;
+                    }
+                    break :blk .{ .tag = Tag.map, .data = .{ .list = .{ .items = list.ptr, .count = @intCast(count) } } };
                 },
                 else => Nil,
             },
@@ -46,12 +71,16 @@ pub const ExportValue = extern struct {
     }
 
     pub fn deinit(self: *const ExportValue, allocator: std.mem.Allocator) void {
-        if (self.tag == .list) {
-            for (self.data.list.items[0..self.data.list.count]) |item| {
-                item.deinit(allocator);
-            }
-            allocator.free(self.data.list.items[0..self.data.list.count]);
+        switch (self.tag) {
+            .list, .set, .map => {
+                var count = self.data.list.count;
+                if (self.tag == .map) count *= 2;
+                for (self.data.list.items[0..self.data.list.count]) |item| {
+                    item.deinit(allocator);
+                }
+                allocator.free(self.data.list.items[0..self.data.list.count]);
+            },
+            else => {},
         }
     }
-
 };
