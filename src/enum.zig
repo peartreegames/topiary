c471e2993ffb48bc22a65c8bcaf901d2e22942ec
bgk- <blakegraysonkehler@gmail.com>
2024-02-25 21:21:00 -0800

    Refactor enums

--- src/enum.zig
+++ src/enum.zig
@@ -3,11 +3,10 @@ const std = @import("std");
 pub const Enum = struct {
     allocator: std.mem.Allocator,
     name: []const u8,
-    values: []Value,
+    values: [][]const u8,
 
     pub const Value = struct {
-        index: usize,
-        name: []const u8,
-        base: *Enum,
+        index: u8,
+        base: *const Enum,
     };
 };
